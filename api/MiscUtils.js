06d04c0d71bca3dbd00767a4346457a91b3abf69
willm62 <will.migdol@gmail.com>
2022-10-22 11:58:00 -0700

    started implementing encryption

--- api/MiscUtils.js
+++ api/MiscUtils.js
@@ -2,6 +2,8 @@
  * Misc. utility functions 
  */
 
+const crypto = require('crypto');
+
 module.exports.randomStr = (strLen) => {
     const validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
     let result = '';
@@ -21,4 +23,23 @@ module.exports.apiResponse = (statusPassed, data) => {
         status: statusPassed ? 'PASS' : 'FAIL',
         data
     }
+}
+
+
+/*
+    Putting on hold for now, trying to get encrypting and decrypting strings setup
+    For storing passwords in the db
+*/
+module.exports.encrypt = (phrase) => {
+    const cipher = crypto.createCipher('aes-192-gcm', process.env.JWT_KEY);
+    let encrypted = cipher.update(phrase, 'utf8', 'hex');
+    encrypted+=cipher.final('hex');
+    return encrypted;
+}
+
+module.exports.decrypt = (phrase) => {
+    const cipher = crypto.createDecipher('aes-192-gcm', process.env.JWT_KEY);
+    let decrypted = cipher.update(phrase, 'utf8', 'hex');
+    decrypted+=cipher.final('hex');
+    return decrypted;
 }
