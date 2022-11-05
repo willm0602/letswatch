ac4529f5dd623b0871d8322288585302873c1dcd
willm62 <will.migdol@gmail.com>
2022-11-05 14:58:58 -0700

    added update for when user logs in/signs out

--- frontend/src/LocalStorageInterface.js
+++ frontend/src/LocalStorageInterface.js
@@ -2,36 +2,33 @@
  * Functions to help w/ interacting with local storage
  */
 
-function reload() {
-    window.location.reload()
+function reload(){
+    window.location.reload();
 }
 
 export function setAccessToken(accessToken) {
-    localStorage.setItem('letswatchAccessToken', accessToken)
-    reload()
+    localStorage.setItem('letswatchAccessToken', accessToken);
+    reload();
 }
 
 export function setID(id) {
-    localStorage.setItem('letswatchID', id)
-    reload()
+    localStorage.setItem('letswatchID', id);
+    reload();
 }
 
 export function getAccessToken() {
     return localStorage.getItem('letswatchAccessToken')
-    reload()
 }
 
 export function deleteAccessToken() {
-    localStorage.removeItem('letswatchAccessToken')
-    reload()
+    localStorage.removeItem('letswatchAccessToken');
+    reload();
 }
 
 export function getID() {
     return localStorage.getItem('letswatchID')
-    reload()
 }
 
 export function userIsSignedIn() {
     return getAccessToken() != null
-    reload()
 }
